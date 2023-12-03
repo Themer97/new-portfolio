46d1424cf9c2ea659d44ed80cd261e7245fba646
Themer97 <themersaanouni@gmail.com>
2023-12-03 15:06:43 +0100

    finishing integration of experience and contact sections

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,39 +1,46 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
-import { HiDownload } from 'react-icons/hi';
-import { FaGithub } from 'react-icons/fa';
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithub } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+
   return (
-    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
-      <div className='flex items-center justify-center'>
-        <div className='relative'>
+    <section
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      ref={ref}
+    >
+      <div className="flex items-center justify-center">
+        <div className="relative">
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ type: 'tween', duration: 0.2 }}
+            transition={{ type: "tween", duration: 0.2 }}
           >
             <Image
-              src='https://images.unsplash.com/photo-1700448170710-df7b4dcd756e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+              src="https://images.unsplash.com/photo-1700448170710-df7b4dcd756e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="Themer's portfolio"
-              width='192'
-              height='192'
-              quality='95'
+              width="192"
+              height="192"
+              quality="95"
               priority={true}
-              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
           <motion.span
-            className='absolute bottom-0 right-0 text-4xl'
+            className="absolute bottom-0 right-0 text-4xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
-              type: 'spring',
+              type: "spring",
               stiffness: 125,
               delay: 0.1,
               duration: 0.7,
@@ -44,19 +51,19 @@ export default function Intro() {
         </div>
       </div>
       <motion.h1
-        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className='font-bold'>Hello, I&apos;m Themer.</span> I&apos;m a
-        <span className='font-bold'> front-end developer</span> with
-        <span className='font-bold'> one and a half years</span> of experience.
-        I enjoy building <span className='italic'>sites & apps</span>. My focus
+        <span className="font-bold">Hello, I&apos;m Themer.</span> I&apos;m a
+        <span className="font-bold"> front-end developer</span> with
+        <span className="font-bold"> one and a half years</span> of experience.
+        I enjoy building <span className="italic">sites & apps</span>. My focus
         is
-        <span className='underline'> React (Next.js)</span>.
+        <span className="underline"> React (Next.js)</span>.
       </motion.h1>
       <motion.div
-        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -64,31 +71,31 @@ export default function Intro() {
         }}
       >
         <Link
-          href='#contact'
-          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
         >
           Contact me here
-          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
-          href='/CV.pdf'
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/CV.pdf"
           download
         >
           Download CV
-          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
-          className='bg-white p-4 hover:text-gray-950 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
-          href='https://www.linkedin.com/in/themer-saanouni-2590a619a/'
-          target='_blank'
+          className="bg-white p-4 hover:text-gray-950 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/themer-saanouni-2590a619a/"
+          target="_blank"
         >
           <BsLinkedin />
         </a>
         <a
-          className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10'
-          href='https://github.com/Themer97'
-          target='_blank'
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/Themer97"
+          target="_blank"
         >
           <FaGithub />
         </a>
--- lib/data.ts
+++ lib/data.ts
@@ -1,107 +1,103 @@
-import React from 'react';
-import { CgWorkAlt } from 'react-icons/cg';
-import { FaReact } from 'react-icons/fa';
-import { LuGraduationCap } from 'react-icons/lu';
-import corpcommentImg from '@/public/corpcomment.png';
-import rmtdevImg from '@/public/rmtdev.png';
-import wordanalyticsImg from '@/public/wordanalytics.png';
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
-    name: 'Home',
-    hash: '#home',
+    name: "Home",
+    hash: "#home",
   },
   {
-    name: 'About',
-    hash: '#about',
+    name: "About",
+    hash: "#about",
   },
   {
-    name: 'Projects',
-    hash: '#projects',
+    name: "Projects",
+    hash: "#projects",
   },
   {
-    name: 'Skills',
-    hash: '#skills',
+    name: "Skills",
+    hash: "#skills",
   },
   {
-    name: 'Experience',
-    hash: '#experience',
+    name: "Experience",
+    hash: "#experience",
   },
   {
-    name: 'Contact',
-    hash: '#contact',
+    name: "Contact",
+    hash: "#contact",
   },
 ] as const;
 
 export const experiencesData = [
   {
-    title: 'Graduated bootcamp',
-    location: 'Miami, FL',
-    description:
-      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+    title: "Graduated Geomatics(GIS) engineering",
+    location: "Tunis",
+    description: "I'm an engineer in landsurveying and geomatics.",
     icon: React.createElement(LuGraduationCap),
-    date: '2019',
+    date: "2022",
   },
   {
-    title: 'Front-End Developer',
-    location: 'Orlando, FL',
+    title: "Graduated bootcamp",
+    location: "Tunis",
     description:
-      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+      "I graduated after 6 months of studying. I was an intern in the company that i'm working for now for the whole bootcamp period",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Menzah, Ariana",
+    description:
+      "I'am working as a front-end developer for 1 year in this job.",
     icon: React.createElement(CgWorkAlt),
-    date: '2019 - 2021',
-  },
-  {
-    title: 'Full-Stack Developer',
-    location: 'Houston, TX',
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: '2021 - present',
+    date: "currently",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: 'Unidrive',
+    title: "Unidrive",
     description:
-      'I worked as a front-end developer on this project for 6 months. Users can give public feedback to companies.',
-    tags: ['React', 'Next.js', 'Sass', 'Redux Toolkit'],
+      "I worked as a front-end developer on this project for 6 months. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "Sass", "Redux Toolkit"],
     imageUrl: corpcommentImg,
   },
   {
-    title: 'rmtDev',
+    title: "rmtDev",
     description:
-      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
-    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
     imageUrl: rmtdevImg,
   },
   {
-    title: 'Word Analytics',
+    title: "Word Analytics",
     description:
-      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
-    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
     imageUrl: wordanalyticsImg,
   },
 ] as const;
 
 export const skillsData = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Next.js',
-  'Node.js',
-  'Git',
-  'Tailwind',
-  'Prisma',
-  'MongoDB',
-  'Redux',
-  'GraphQL',
-  'Apollo',
-  'Express',
-  'PostgreSQL',
-  'Python',
-  'Django',
-  'Framer Motion',
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Sass",
+  "MongoDB",
+  "Redux",
+  "Redux Toolkit",
+  "GraphQL",
+  "Express",
+  "PostgreSQL",
+  "Framer Motion",
 ] as const;
