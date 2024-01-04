f5682fd992c50d2da5a42278f4c51da00126f809
Themer97 <themersaanouni@gmail.com>
2024-01-04 20:38:19 +0100

    fixed the problem for the intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -24,93 +24,96 @@ export default function Intro() {
     >
       <div className="flex items-center justify-center">
         <div className="relative">
+          <div className="relative flex items-center justify-center">
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "tween",
+                duration: 0.2,
+              }}
+            >
+              <Image
+                src={profilePic}
+                alt="Themer's portfolio"
+                width="220"
+                height="220"
+                quality="95"
+                priority={true}
+                className="h-36 w-36 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              />
+            </motion.div>
+            <motion.span
+              className="absolute bottom-0 right-80 text-4xl"
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 125,
+                delay: 0.1,
+                duration: 0.7,
+              }}
+            >
+              👋
+            </motion.span>
+          </div>
+
+          <motion.h1
+            className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+          >
+            <span className="font-bold">Hello, I&apos;m Themer.</span> I&apos;m
+            a<span className="font-bold"> front-end developer</span> with
+            <span className="font-bold"> one and a half years</span> of
+            experience. I enjoy building{" "}
+            <span className="italic">sites & apps</span>. My focus is
+            <span className="underline"> React (Next.js)</span>.
+          </motion.h1>
           <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
+            className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src={profilePic}
-              alt="Themer's portfolio"
-              width="220"
-              height="220"
-              quality="95"
-              priority={true}
-              className="h-36 w-36 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
-          </motion.div>
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
               delay: 0.1,
-              duration: 0.7,
             }}
           >
-            👋
-          </motion.span>
+            <Link
+              href="#contact"
+              className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+              onClick={() => {
+                setActiveSection(link.name);
+                setTimeOfLastClick(Date.now());
+              }}
+            >
+              Contact me here
+              <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+            </Link>
+            <a
+              className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+              href="/CV.pdf"
+              download
+            >
+              Download CV
+              <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+            <a
+              className="bg-white p-4 hover:text-gray-950 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+              href="https://www.linkedin.com/in/themer-saanouni-2590a619a/"
+              target="_blank"
+            >
+              <BsLinkedin />
+            </a>
+            <a
+              className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+              href="https://github.com/Themer97"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+          </motion.div>
         </div>
       </div>
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <span className="font-bold">Hello, I&apos;m Themer.</span> I&apos;m a
-        <span className="font-bold"> front-end developer</span> with
-        <span className="font-bold"> one and a half years</span> of experience.
-        I enjoy building <span className="italic">sites & apps</span>. My focus
-        is
-        <span className="underline"> React (Next.js)</span>.
-      </motion.h1>
-      <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.1,
-        }}
-      >
-        <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {
-            setActiveSection(link.name);
-            setTimeOfLastClick(Date.now());
-          }}
-        >
-          Contact me here
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
-          href="/CV.pdf"
-          download
-        >
-          Download CV
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
-        <a
-          className="bg-white p-4 hover:text-gray-950 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
-          href="https://www.linkedin.com/in/themer-saanouni-2590a619a/"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
-          href="https://github.com/Themer97"
-          target="_blank"
-        >
-          <FaGithub />
-        </a>
-      </motion.div>
     </section>
   );
 }
