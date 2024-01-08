ec98abc34d9ea8e7e4cc7ee75f351be72aea888d
themer <themer.saanouni@agence-inspire.com>
2024-01-08 17:08:56 +0100

    made some changes

--- lib/data.ts
+++ lib/data.ts
@@ -1,133 +1,133 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { LuGraduationCap } from "react-icons/lu";
-import myInspireImg from "@/public/Splash Screen.png";
-import babyWorldImg from "@/public/login.jpg";
-import unidriveImg from "@/public/unidrive.png";
-import cocktailDbImg from "@/public/cocktailDB.png";
-import SkillsImg from "@/public/Skills-landingPage.png";
-import adminDashboard from "@/public/myAdminDashboard.png";
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { LuGraduationCap } from 'react-icons/lu';
+import myInspireImg from '@/public/Splash Screen.png';
+import babyWorldImg from '@/public/login.jpg';
+import unidriveImg from '@/public/unidrive.png';
+import cocktailDbImg from '@/public/cocktailDB.png';
+import SkillsImg from '@/public/Skills-landingPage.png';
+import adminDashboard from '@/public/myAdminDashboard.png';
 
 export const links = [
   {
-    name: "Home",
-    hash: "#home",
+    name: 'Home',
+    hash: '#home',
   },
   {
-    name: "About",
-    hash: "#about",
+    name: 'About',
+    hash: '#about',
   },
   {
-    name: "Projects",
-    hash: "#projects",
+    name: 'Projects',
+    hash: '#projects',
   },
   {
-    name: "Skills",
-    hash: "#skills",
+    name: 'Skills',
+    hash: '#skills',
   },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: 'Experience',
+    hash: '#experience',
   },
   {
-    name: "Contact",
-    hash: "#contact",
+    name: 'Contact',
+    hash: '#contact',
   },
 ] as const;
 
 export const experiencesData = [
   {
-    title: "Graduated Geomatics(GIS) engineering",
-    location: "Tunis",
+    title: 'Graduated Geomatics(GIS) engineering',
+    location: 'Tunis',
     description: "I'm an engineer in landsurveying and geomatics.",
     icon: React.createElement(LuGraduationCap),
-    date: "2022",
+    date: '2022',
   },
   {
-    title: "Graduated bootcamp",
-    location: "Tunis",
+    title: 'Graduated bootcamp',
+    location: 'Tunis',
     description:
       "I graduated after 6 months of studying. I was an intern in the company that i'm working for now for the whole bootcamp period",
     icon: React.createElement(LuGraduationCap),
-    date: "2023",
+    date: '2023',
   },
   {
-    title: "Front-End Developer",
-    location: "Menzah, Ariana",
+    title: 'Front-End Developer',
+    location: 'Menzah, Ariana',
     description:
       "I'am working as a front-end developer for 1 year in this job.",
     icon: React.createElement(CgWorkAlt),
-    date: "currently",
+    date: 'currently',
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "Unidrive",
+    title: 'Unidrive',
     description:
-      "Driving schools app,where both schools and students interact with each other. I got involved in the implementation and dynamisation of this app.",
-    tags: ["React", "Next.js", "Sass", "Redux Toolkit"],
+      'Driving schools app,where both schools and students interact with each other. I was involved in the implementation and dynamisation of this app.',
+    tags: ['React', 'Next.js', 'Sass', 'Redux Toolkit'],
     imageUrl: unidriveImg,
-    projectLink: "https://unidrive.app/",
+    projectLink: 'https://unidrive.app/',
   },
   {
-    title: "MyInspire",
+    title: 'MyInspire',
     description:
-      "internal company app. We can using this app demand daysoff, remote work or authorizations. Also we can check our balances,I got in charge of the web and mobile app.",
-    tags: ["React-native", "TypeScript", "Tailwind", "Redux"],
+      'internal company app. We can using this app demand daysoff, remote work or authorizations. Also we can check our balances,I was in charge of the web and mobile app.',
+    tags: ['React-native', 'TypeScript', 'Tailwind', 'Redux'],
     imageUrl: myInspireImg,
   },
   {
-    title: "Baby World",
+    title: 'Baby World',
     description:
-      "A tracking app for pharmaceutical company to track their medical representatives. I got involved in the implementation and dynamisation of this app.",
-    tags: ["React-native", "TypeScript", "Tailwind", "Redux", "Framer"],
+      'A tracking app for pharmaceutical company to track their medical representatives. I got involved in the implementation and dynamisation of this app.',
+    tags: ['React-native', 'TypeScript', 'Tailwind', 'Redux', 'Framer'],
     imageUrl: babyWorldImg,
   },
   {
-    title: "CocktailDB",
+    title: 'CocktailDB',
     description:
-      "personal project to get a better understanding of Redux Toolkit using free api from theCocktailDB",
-    tags: ["NextJs", "JavaScript", "AntD", "ReduxToolkit"],
+      'personal project to get a better understanding of Redux Toolkit using free api from theCocktailDB',
+    tags: ['NextJs', 'JavaScript', 'AntD', 'ReduxToolkit'],
     imageUrl: cocktailDbImg,
-    projectLink: "https://themer-cocktail-db-chh.vercel.app/",
+    projectLink: 'https://themer-cocktail-db-chh.vercel.app/',
   },
   {
-    title: "Admin Dashboard",
+    title: 'Admin Dashboard',
     description:
-      "This a fullstack project, an admin dashboard to track employees. I utilize the CRUD opertaions and authentication in this project.",
-    tags: ["React", "Redux", "antD", "NodeJs", "Express", "MongoDB"],
+      'This a fullstack project, an admin dashboard to track employees. I utilize the CRUD opertaions and authentication in this project.',
+    tags: ['React', 'Redux', 'antD', 'NodeJs', 'Express', 'MongoDB'],
     imageUrl: adminDashboard,
     projectLink:
-      "https://dashboard-users-git-main-themer-saanounis-projects.vercel.app/",
+      'https://dashboard-users-git-main-themer-saanounis-projects.vercel.app/',
   },
   {
-    title: "Skills",
+    title: 'Skills',
     description:
       "This is one of my first projects, it's a landing page for a company called Skills. I used a lot of CSS and JavaScript to make this project.",
-    tags: ["React", "JavaScript", "Bootstrap"],
+    tags: ['React', 'JavaScript', 'Bootstrap'],
     imageUrl: SkillsImg,
-    projectLink: "https://test-inspire.vercel.app/",
+    projectLink: 'https://test-inspire.vercel.app/',
   },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "React-Native",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "Sass",
-  "MongoDB",
-  "Redux",
-  "Redux Toolkit",
-  "GraphQL",
-  "Express",
-  "PostgreSQL",
-  "Framer Motion",
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'React-Native',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'Sass',
+  'MongoDB',
+  'Redux',
+  'Redux Toolkit',
+  'GraphQL',
+  'Express',
+  'PostgreSQL',
+  'Framer Motion',
 ] as const;
